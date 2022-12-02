63ce17ec0a2e78a72b5713c9d44bbd426dcef487
Sinan Alagöz <sinanalagoz74@gmail.com>
2022-12-02 18:03:32 +0300

    33

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_project/home_page.dart';
@@ -6,11 +9,23 @@ import 'package:hive_project/model.dart';
 
 void main() async {
   await Hive.initFlutter('depo');
+
+  var secureStorage = const FlutterSecureStorage();
+  var containsEncryptionKey = await secureStorage.containsKey(key: 'key');
+  if (!containsEncryptionKey) {
+    var key = Hive.generateSecureKey();
+    await secureStorage.write(key: 'key', value: base64UrlEncode(key));
+  }
+
+  var encryptionKey =
+      base64Url.decode(await secureStorage.read(key: 'key') ?? 'newKey');
+  debugPrint("encryptionKey: $encryptionKey");
   //  await Hive.openBox('test');
   Hive.registerAdapter(StudentAdapter());
 
   // await Hive.openBox<Student>('students');
-  await Hive.openLazyBox<Student>('students1');
+ await Hive.openLazyBox<Student>('students1', encryptionCipher: HiveAesCipher(encryptionKey));
+  // await Hive.openLazyBox<Student>('students1');
   runApp(const MyApp());
 }
 
